961a9fc14d36f3db32d3cd9ff93cfb680161f2c0
tgsirk <kris.zxc@gmail.com>
2022-07-05 23:37:51 +0200

    WIP

--- reactions/next.js
+++ reactions/next.js
@@ -2,6 +2,7 @@ module.exports= {
     async execute(reaction, client) {
         const queue = await client.player.getQueue(reaction.message.guild);
         if (!queue) return;
+        if (queue.tracks.length === 0) return;
         queue.skip()
     }
 }
---
+++ reactions/stop.js
@@ -0,0 +1,8 @@
+module.exports= {
+    async execute(reaction, client) {
+        const queue = await client.player.getQueue(reaction.message.guild);
+        if (!queue) return;
+        queue.clear();
+        queue.stop();
+    }
+}
